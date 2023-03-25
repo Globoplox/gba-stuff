fbd61d7cc7b5eaa66ccc3fd50c81884cbc8e17bd
globoplox <globoplox@gmail.com>
2023-03-25 17:22:54 +0100

    Copy tilemap

--- src/game/splash.cr
+++ src/game/splash.cr
@@ -9,10 +9,18 @@ require "../state"
 #   end
 # end
 
-{% begin %}
+# Make it a macro, make the generated bindings easier to use,
+# add load simple routine (maybe mater dma one idk how it works yet)
+# Make the tiles 4bpp (this mean: checking no tile has more than 16 color, and all color used by tiles must be in a 16 block, and if many blocks in palette,
+# they must be 16b aligned).
+
+# HOW TO: for each tile, generate a palette? Must not exceed 16 entries.
+# Make a group of tile for each tile, linked to the palette.
+# FOr each group (currenly of one tile), merge it into the first group whose palette is a superset of the current.
+# Then, take the group with the biggest palette, and try to find the biggest group that could fit if merged (a_pal_size + b_pal_size - common_pal_size <16).
+# repeat until one pass happen without any merge. (could work by generating permutation and testing them until one good is found, then retrying)
+# Now you have n group, each with their palette.
 {{ `crystal ./src/compile_time/assets_links.cr minimal` }}
-{% debug() %}
-{% end %}
 
 module Splash
   extend self
@@ -48,7 +56,9 @@ module Splash
 
   def draw
     # set ONE tile in the current drawn tilemap
-    ((pointerof(GBA::Screen::HAL.vram).as(GBA::Screen::HAL::Tilemap*) + 31).as(UInt16*) + (@@i >> 3)).value = (@@i >> 3).to_u16! # the most basic tilemap entry: index into the tilemap
+    # ((pointerof(GBA::Screen::HAL.vram).as(GBA::Screen::HAL::Tilemap*) + 31).as(UInt16*) + (@@i >> 3)).value = (@@i >> 3).to_u16!
+    ((pointerof(GBA::Screen::HAL.vram).as(GBA::Screen::HAL::Tilemap*) + 31).as(UInt32*) + (@@i >> 3)).value = pointerof(Assets::Minimal.map_start)[@@i >> 3]
+    # the most basic tilemap entry: index into the tilemap
     #GBA::Screen::Mode3[@@i] = 0x0ff0u16
   end
 end
