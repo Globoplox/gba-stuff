edab77350985cbebd202a2986437958bbdc04248
globoplox <globoplox@gmail.com>
2023-04-01 22:44:25 +0200

    Fix base palette

--- src/compile_time/txt_to_palette.cr
+++ src/compile_time/txt_to_palette.cr
@@ -10,11 +10,14 @@ unless ARGV[1]?
 end
 
 File.open ARGV[1], "w" do |output|
+  i = 0
   File.open ARGV[0], "r" do |input|
     input.each_line do |line|
       next if line.empty?
       raise "Bad palette entry format: '#{line}'" unless line =~ /^(0x[0-9a-fA-F_]+|0b[01_]+)/
       $1.to_u16(prefix: true, underscore: true).to_io output, IO::ByteFormat::LittleEndian
+      i += 1
     end
   end
+  0u16.to_io output, IO::ByteFormat::LittleEndian if i.odd?  
 end
